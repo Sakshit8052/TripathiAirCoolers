74a88c93909aee10ea39012c0ce062ae1e3ff39d
sakshi8052.github.io <tripathimr36@gmail.com>
2026-02-06 01:52:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-// Simple alert on enquiry submit
-document.getElementById("enquiryForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Thank you! Your enquiry has been submitted.");
-    this.reset();
-});
+function toggleDark(){
+document.body.classList.toggle("dark");
+}
